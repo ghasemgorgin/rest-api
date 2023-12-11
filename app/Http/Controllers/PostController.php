5ddecbf348c3a8ae0eaaafaebfa68495997875f4
ghasemgorgin <ghasemgorgin74@gmail.com>
2023-12-11 20:42:57 +0330

    end of resorce

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,13 @@ class PostController extends Controller
 {
    public function index(){
         $post=Post::all();
-         return response()->json(['data'=>$post]);
-        
+        //  return response()->json(['data'=>$post]);
+        return PostResource::collection($post);
+   }
+   public function show($id){
+    $posts=Post::findOrfail($id);
+    
+    return  new PostResource($posts);
+
    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::get('/post',[PostController::class, 'index']);
+Route::get('/post/{id}',[PostController::class, 'show']);
 
 
 
